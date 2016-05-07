2e4d0dc981a09e414323d6840b418f6dfcaa5245
saxsoares <sax.soares@gmail.com>
2016-05-07 15:57:16 -0300

    funÃ§Ã£o delta_X

--- PROJETO3.c
+++ PROJETO3.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 #define W 0.1
-
-
+#define alfa 6.0
+#define beta 0.1
+#define theta 0.5
+#define epsilon 0.02
+#define rho 0
+#define I_at 0.2
+#define I_des -0.2
 
 struct no {
-	float x;
-	float y;
+	double x;
+	double y;
 };
 
 typedef struct no no;
 
-float S(int matrix[500][500], no no[500], int linha){
+double S(int matrix[500][500], no no[500], int linha){
     int i, j;
-    float soma = 0;
+    double soma = 0;
 
     for(i = 0; i < 500; i++){
         if(matrix[linha][i] == 1 && no[linha].x > 0){
@@ -24,11 +30,17 @@ float S(int matrix[500][500], no no[500], int linha){
     return soma;
 }
 
+double delta_x(int matriz[500][500], no neuronios[500], int id){
+        double deltaX;
+        deltaX = 3 * neuronios[id].x - pow(neuronios[id].x, 3) + 2 - neuronios[id].y  + (double)I_at + S(matriz, neuronios, id);
+
+        return deltaX;
+}
 
 void main(){
     int matriz[500][500];
     int i, j;
-    float vet_S[500];
+    double vet_S[500];
     no neuronios[500];
 
     for(i = 0; i < 500; i++){
