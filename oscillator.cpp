eb940ea3feabe7951b885aa958101c3ee262fc99
FernandoSoares <sax.soares@gmail.com>
2016-05-12 21:39:11 -0300

    Add files via upload

--- oscillator.cpp
+++ oscillator.cpp
@@ -5,14 +5,21 @@
 #define MAX 500 // QUILES
 #define DT 0.01 // QUILES
 
-#define W 0.1;
-#define alfa 6.0
+double W  = 0.1;
+double alfa  = 6.0;
+double beta  = 0.1;
+double theta  = 0.5;
+double epsilon  = 0.02;
+double rho  = 0;
+double I_at  = 0.2;
+double I_des  = -0.02;
+/*#define alfa 6.0
 #define beta 0.1
 #define theta 0.5
 #define epsilon 0.02
 #define rho 0
 #define I_at 0.2
-#define I_des -0.2
+#define I_des -0.2*/
 
 struct no {
 	double x;
@@ -39,14 +46,15 @@ void carrega_matriz(int matriz[MAX][MAX]){
         matriz[col][lin] = 1;
     }
 
-    for(k = 0; k < 100; k++)
-        for(i = 0; i < MAX; i++){
-            for(j = 0; j< MAX; j++){
-                printf("%d\n", matriz[i][j]);
-            }
-            printf("\n");
-        }
+//    for(k = 0; k < 100; k++)
+//        for(i = 0; i < MAX; i++){
+//            for(j = 0; j< MAX; j++){
+//                printf("%d\n", matriz[i][j]);
+//            }
+//            printf("\n");
+//        }
 }
+
 
 double S(int matrix[MAX][MAX], Tno neu[MAX], int linha){
     int i;
@@ -64,7 +72,8 @@ double delta_x(int matriz[MAX][MAX], Tno neuronios[MAX], int id){
         double deltaX;
     // QUILES - em equações envolvendo ponto flutuante, evitar o uso de inteiros, por exemplo, na equacao abaixo, substitui 3 por 3.0, etc.
     // QUILES - Faltou o passo de integração DT
-        deltaX = (3.0 * neuronios[id].x - pow(neuronios[id].x, 3.0) + 2.0 - neuronios[id].y  + I_at)*DT + S(matriz, neuronios, id);
+        deltaX = (3.0 * neuronios[id].x - pow(neuronios[id].x, 3) + 2.0 - neuronios[id].y  + I_at)*DT /*+ S(matriz, neuronios, id)*/;
+        //printf("neuronio y: %.2f\n", neuronios[id].y);
         return deltaX;
 }
 
@@ -83,28 +92,46 @@ int main(){
     Tno neuronios[500];
     Tno armaz[500];
 
-    carrega_matriz(matriz);
     //Valores Iniciais do Neurônio
+// QUILES    neuronios[0].x=10;
+// QUILES    neuronios[0].y=1000;
+    //neuronios[1].x=1;
+    //neuronios[1].y=0;
+
+    carrega_matriz(matriz);
+    // Quiles
     for (i=0 ; i<500 ; i++){
         neuronios[i].x = -2.0 + (double)(rand()%2001) / 500.0; // valor entre -2 e 2;
-        neuronios[i].y = (double)(rand()%2001) / 500.0; // valor entre 0 e 4;
+        neuronios[i].x = (double)(rand()%2001) / 500.0; // valor entre 0 e 4;
     }
 
-/*
+//    matriz[0][0]=0;
+//    matriz[0][1]=1;
+//    matriz[1][0]=1;
+//    matriz[1][1]=0;
+
     for(t=0;t<1000000;t++){
         for(i = 0; i < 500; i++){
-            neuronios[i].S = S(matriz, neuronios, i);
+            vet_S[i] = S(matriz, neuronios, i);
             armaz[i].x = neuronios[i].x;
             armaz[i].y = neuronios[i].y;
             neuronios[i].x += delta_x(matriz,armaz,i);
             neuronios[i].y += delta_y(matriz,armaz,i);
         }
+//        vet_S[1] = S(matriz, neuronios, 1);
+//
+//        armaz[1].x = neuronios[1].x;
+//        armaz[1].y = neuronios[1].y;
+//
+//
+//        neuronios[1].x += delta_x(matriz,armaz,1);
+//        neuronios[1].y += delta_y(matriz,armaz,1);
         printf("x_0 = %.2f\n", neuronios[0].x);
 //        printf("y_0 = %.2f\n", neuronios[0].y);
         printf("x_1 = %.2f\n", neuronios[1].x);
         //printf("y_1 = %.2f\n", neuronios[1].y);
     }
-*/
+
 
     return 0;
 }
