9fe5d7391ed6784c54ca3626ea6d4b7dc97ec05a
saxsoares <sax.soares@gmail.com>
2016-05-19 16:35:42 -0300

    Acrescentei alguns arquivos

---
+++ matriz_all.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+#define MAX 500 // QUILES
+#define DT 0.05 // QUILES
+
+int main(){
+    int i, j;
+    for(i = 0; i< 500; i++){
+        for(j = 0; j < 500; j++){
+            if(i == j) continue;
+            printf("%d %d\n", i, j);
+        }
+    }
+}
